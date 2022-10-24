2e07422ee535b7ba61263d98bd83ba9b10ad0ca2
Alessandro Arici <alessandro.arici@gmail.com>
2022-10-24 21:12:58 +0200

    Complete versions route

--- tests/Feature/Controllers/VersionsControllerTest.php
+++ tests/Feature/Controllers/VersionsControllerTest.php
@@ -7,10 +7,12 @@ use Tests\TestCase;
 
 class VersionsControllerTest extends TestCase
 {
-    public function test_can_return_ok()
+    public function test_can_return_version()
     {
         $response = $this->get(Route('versions_index'));
         $response->assertStatus(200);
-        $response->assertContent("ok");
+        $response->assertJson([
+            "version" => "9.8.7"
+        ]);
     }
 }
